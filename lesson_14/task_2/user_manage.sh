c0092d9aed2b6bc6a36de75341d86a14a7273aaf
Aleksei Russakov <87088944+BruxBel@users.noreply.github.com>
2025-11-05 09:42:56 +0300

    Update user_manage.sh

--- lesson_14/task_2/user_manage.sh
+++ lesson_14/task_2/user_manage.sh
@@ -3,88 +3,111 @@
 #Checking root permissions
 if [ "$(id -u)" != 0 ]
 then
-  echo root permissions required >&2
+  echo "Root permissions required" >&2
   exit 1
 fi
 
 #Variables
-file=/var/users.csv
-oldIFS=$IFS
+file="/var/users.csv"
+oldIFS="$IFS"
 
 #Functions
 check_user() {
-i=1
-while cut -d: -f1 /etc/passwd | grep ^$user$ > /dev/null
-do 
-   user=$user$i
-   ((i++))
-done
+    local i=1
+    local original_user="$user"
+    while cut -d: -f1 /etc/passwd | grep -q "^$user$"
+    do 
+        user="${original_user}${i}"
+        ((i++))
+    done
 }
 
 create_user() {
-#Restore IFS
-IFS=$oldIFS
-shell=/sbin/nologin
-#Create group
-groupadd $group
+    #Restore IFS
+    IS="$oldIFS"
+    local shell="/sbin/nologin"
+    
+    #Create group if not exists
+    if ! grep -q "^$group:" /etc/group; then
+        groupadd "$group"
+    fi
 
-# Sudoers
-if [ "$group" = it ] || [ "$group" = security ]
-then
-  if ! grep "%$group" /etc/sudoers
-    then 
-    cp /etc/sudoers{,.bkp}
-    echo '%'$group' ALL=(ALL) ALL' >> /etc/sudoers
-  fi
-  shell=/bin/bash
-elif [ "$user" = admin ]
-then
-  if ! grep "$user" /etc/sudoers
-    then 
-    cp /etc/sudoers{,.bkp}
-    echo $user' ALL=(ALL) ALL' >> /etc/sudoers
-  fi
-  shell=/bin/bash
-fi
+    # Sudoers configuration
+    if [ "$group" = "it" ] || [ "$group" = "security" ]; then
+        if ! grep -q "^%$group" /etc/sudoers; then
+            cp /etc/sudoers /etc/sudoers.bkp
+            echo "%$group ALL=(ALL) ALL" >> /etc/sudoers
+        fi
+        shell="/bin/bash"
+    elif [ "$user" = "admin" ]; then
+        if ! grep -q "^$user" /etc/sudoers; then
+            cp /etc/sudoers /etc/sudoers.bkp
+            echo "$user ALL=(ALL) ALL" >> /etc/sudoers
+        fi
+        shell="/bin/bash"
+    fi
 
-mkdir /home/$group
-useradd $user -g $group -b /home/$group -s $shell -c "Birthday $bday"
+    # Create home directory if doesn't exist
+    if [ ! -d "/home/$group" ]; then
+        mkdir -p "/home/$group"
+    fi
+    
+    # Create user
+    useradd "$user" -g "$group" -b "/home/$group" -s "$shell" -c "Birthday $bday"
+    
+    # Set secure permissions on home directory
+    chmod 755 "/home/$group"
 }
 
-
 #Check parameters
-if [ ! -z $2 ]
-then
-  user=$1
-  group=$2
-  echo Username: $user   Group: $group
-  create_user
-elif [ -f $file ]
-then
-  IFS=$'\n'
-  for line in $(cut -d, -f2,3,4,5 $file | tail -n +2 | tr '[:upper:]' '[:lower:]' | tr , ' ' )
-  do
-    user=$(echo $(echo "$line" | cut -c1).$(echo "$line" | cut -d' ' -f2))
-    group=$(echo "$line" | cut -d' ' -f4)
-    bday=$(echo "$line" | cut -d' ' -f3)
+if [ $# -eq 2 ]; then
+    user="$1"
+    group="$2"
+    echo "Username: $user   Group: $group"
     check_user
-    echo Username: $user Group: $group
     create_user
-  done
+elif [ -f "$file" ]; then
+    IFS=$'\n'
+    for line in $(tail -n +2 "$file" | tr '[:upper:]' '[:lower:]')
+    do
+        # Proper CSV parsing
+        first_name=$(echo "$line" | cut -d, -f2)
+        last_name=$(echo "$line" | cut -d, -f3)
+        bday=$(echo "$line" | cut -d, -f4)
+        group=$(echo "$line" | cut -d, -f5)
+        
+        # Remove carriage returns and trim spaces
+        first_name=$(echo "$first_name" | tr -d '\r' | xargs)
+        last_name=$(echo "$last_name" | tr -d '\r' | xargs)
+        group=$(echo "$group" | tr -d '\r' | xargs)
+        bday=$(echo "$bday" | tr -d '\r' | xargs)
+        
+        user=$(echo "${first_name:0:1}.$last_name" | tr -d ' ')
+        check_user
+        echo "Username: $user Group: $group"
+        create_user
+    done
+    IFS="$oldIFS"
 else
-  echo Welcome!
-  select option in "Add user" "Show users" "Exit"
-  do  case $option in
-         "Add user")
-	            read -p "Print username: " user
-		    read -p "Print groupname: " group
-  		    create_user ;;
-         "Show users")
-		    cut -d: -f1 /etc/passwd ;;
-	 "Exit")
-		    break ;;
-	 *) echo Wrong option ;;
-      esac
-  done
-
+    echo "Welcome!"
+    select option in "Add user" "Show users" "Exit"
+    do  
+        case $option in
+            "Add user")
+                read -p "Enter username: " user
+                read -p "Enter groupname: " group
+                check_user
+                create_user 
+                ;;
+            "Show users")
+                cut -d: -f1 /etc/passwd 
+                ;;
+            "Exit")
+                break 
+                ;;
+            *) 
+                echo "Wrong option" 
+                ;;
+        esac
+    done
 fi
